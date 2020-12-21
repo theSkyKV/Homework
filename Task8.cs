d063fbeb168dd71151e5d83498241733fb535803
theSkyKV <63708227+theSkyKV@users.noreply.github.com>
2020-12-21 16:49:48 +0300

    Add files via upload

---
+++ Task8.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userMessage;
+            int printCount;
+
+            Console.Write("Enter your message: ");
+            userMessage = Console.ReadLine().ToString();
+            Console.Write("Enter print count: ");
+            printCount = Convert.ToInt32(Console.ReadLine());
+
+            for (var i = 0; i < printCount; i++)
+            {
+                Console.WriteLine(userMessage);
+            }
+        }
+    }
+}
