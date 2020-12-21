631b3c80b881c5a64fbcc0560ce599a638aa09e0
theSkyKV <63708227+theSkyKV@users.noreply.github.com>
2020-12-21 12:30:26 +0300

    Add files via upload

---
+++ Task6.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string workplace;
+            int age;
+
+            Console.Write("Hello! What is your name? ");
+            name = Console.ReadLine().ToString();
+            Console.Write("How old are you? ");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Where do you work? ");
+            workplace = Console.ReadLine().ToString();
+
+            Console.Write($"Your name is {name}. You are {age} years old. You work in {workplace}.");
+        }
+    }
+}
