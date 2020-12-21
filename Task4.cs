f6dd824367a6333891947fd4da63430e80b65b84
theSkyKV <63708227+theSkyKV@users.noreply.github.com>
2020-12-21 12:21:41 +0300

    Add files via upload

---
+++ Task4.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int coinsCount;
+            int diamondsCount;
+            int diamondPrice = 10;
+            bool enoughCoins;
+
+            Console.Write("Enter your balance: ");
+            coinsCount = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter number of diamonds to buy: ");
+            diamondsCount = Convert.ToInt32(Console.ReadLine());
+
+            enoughCoins = coinsCount >= diamondPrice * diamondsCount;
+            diamondsCount *= Convert.ToInt32(enoughCoins);
+            coinsCount -= diamondPrice * diamondsCount;
+
+            Console.WriteLine($"You have {coinsCount} coins and {diamondsCount} diamonds");
+        }
+    }
+}
