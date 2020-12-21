d0cd27c36e9cfb4c879de2201342af71cea1b311
theSkyKV <63708227+theSkyKV@users.noreply.github.com>
2020-12-21 12:18:48 +0300

    Add files via upload

---
+++ Task3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int picturesInRow = 3;
+            int picturesCount = 52;
+            int fullRowsCount;
+            int picturesCountInIncompleteRow;
+
+            fullRowsCount = picturesCount / picturesInRow;
+            picturesCountInIncompleteRow = picturesCount % picturesInRow;
+
+            Console.WriteLine($"Полных рядов - {fullRowsCount}, картинок в неполном ряду - {picturesCountInIncompleteRow}");
+        }
+    }
+}
