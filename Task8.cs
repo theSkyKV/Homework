2752d254f5a26105f3b2e4cd81ad5ee873e1fc66
theSkyKV <63708227+theSkyKV@users.noreply.github.com>
2020-12-23 10:37:53 +0300

    Add files via upload

---
+++ Task8.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userMessage;
+
+            do
+            {
+                Console.Write("Enter your message or 'exit' to exit: ");
+                userMessage = Console.ReadLine().ToString();
+            } while (userMessage != "exit");
+        }
+    }
+}
