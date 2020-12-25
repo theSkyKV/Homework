8b28581588d1ea6ab0333658987bee2baa599aae
theSkyKV <63708227+theSkyKV@users.noreply.github.com>
2020-12-25 11:39:44 +0300

    Add files via upload

---
+++ Task11.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 7; i <= 98; i += 7)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
