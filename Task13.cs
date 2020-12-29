6048e9502c177a78e2205bac8684624d443103db
theSkyKV <63708227+theSkyKV@users.noreply.github.com>
2020-12-29 10:49:10 +0300

    Add files via upload

---
+++ Task13.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = "12345";
+            string userInput;
+            int attemptsCount = 3;
+
+            while (attemptsCount > 0)
+            {
+                Console.Write("Enter the password: ");
+                userInput = Console.ReadLine();
+
+                if (userInput == password)
+                {
+                    Console.WriteLine("Secret message");
+                    break;
+                }
+                else
+                {
+                    attemptsCount--;
+                    Console.WriteLine($"Access denied. You have {attemptsCount} attempts.");
+                }
+            }
+        }
+    }
+}
