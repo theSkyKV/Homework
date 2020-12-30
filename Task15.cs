3becae20e97acb2139e52d55072002f85e668c71
theSkyKV <63708227+theSkyKV@users.noreply.github.com>
2020-12-30 12:33:48 +0300

    Add files via upload

---
+++ Task15.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = new int[5, 5];
+            int sum = 0;
+            int mult = 1;
+
+            Random rand = new Random();
+
+            for (var i = 0; i < array.GetLength(0); i++)
+            {
+                for (var j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = rand.Next(0, 10);
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            for (var i = 0; i < array.GetLength(1); i++)
+            {
+                sum += array[2, i];
+            }
+
+            for (var i = 0; i < array.GetLength(0); i++)
+            {
+                mult *= array[i, 1];
+            }
+
+            Console.WriteLine($"Сумма - {sum}, произведение - {mult}");
+        }
+    }
+}
