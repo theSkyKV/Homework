df183925ced562e88182947b06439aa07fe9ac2d
theSkyKV <63708227+theSkyKV@users.noreply.github.com>
2021-01-18 20:00:03 +0300

    Add files via upload

---
+++ Task20.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace CSLight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value;
+            int maxValue;
+            int position;
+            while (true)
+            {
+                Console.Write("Введите значение: ");
+                int.TryParse(Console.ReadLine(), out value);
+                Console.Write("Введите максимальное значение: ");
+                int.TryParse(Console.ReadLine(), out maxValue);
+                Console.Write("Введите позицию: ");
+                int.TryParse(Console.ReadLine(), out position);
+                DrawBar(value, maxValue, position);
+            }
+        }
+
+        static void DrawBar(int value, int maxValue, int position = 1)
+        {
+            Console.Clear();
+            Console.SetCursorPosition(0, position);
+            
+            if(value < 0 || maxValue < 0 || position < 0 || value > maxValue)
+            {
+                Console.WriteLine("Некорректное значение");
+                return;
+            }
+
+            Console.Write("[");
+            for (var i = 0; i < value; i++)
+            {
+                Console.Write("#");
+            }
+            for (var i = value; i < maxValue; i++)
+            {
+                Console.Write("_");
+            }
+            Console.Write("]");
+            Console.WriteLine();
+        }
+    }
+}
