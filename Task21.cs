7ea38950ac761adf67f12e10af488bb373279d74
theSkyKV <63708227+theSkyKV@users.noreply.github.com>
2021-01-18 20:18:08 +0300

    Add files via upload

---
+++ Task21.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CSLight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetNumber());
+        }
+
+        static int GetNumber()
+        {
+            int userInput;
+            do
+            {
+                Console.Write("Введите целое число: ");
+            } while (!int.TryParse(Console.ReadLine(), out userInput));
+
+            return userInput;
+        }
+    }
+}
