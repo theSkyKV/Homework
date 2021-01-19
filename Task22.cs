39d7b6c5dc32887718ad2992f7a75dce8c938d93
theSkyKV <63708227+theSkyKV@users.noreply.github.com>
2021-01-19 16:50:33 +0300

    Add files via upload

---
+++ Task22.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace CSLight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 0, 1, 2, 3, 4, 5, 6, 7 };
+
+            Shuffle(ref numbers);
+            foreach (var number in numbers)
+            {
+                Console.Write(number + " ");
+            }
+        }
+
+        static void Shuffle(ref int[] array)
+        {
+            Random rand = new Random();
+            int[] newIndexes = new int[0];
+
+            for (var i = 0; i < array.Length; i++)
+            {
+                int[] tempArray = new int[newIndexes.Length + 1];
+                for (var j = 0; j < newIndexes.Length; j++)
+                {
+                    tempArray[j] = newIndexes[j];
+                }
+                int index = rand.Next(0, tempArray.Length);
+                tempArray[tempArray.Length - 1] = index;
+                newIndexes = tempArray;
+            }
+
+            for (var i = 0; i < newIndexes.Length; i++)
+            {
+                Console.Write(newIndexes[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
