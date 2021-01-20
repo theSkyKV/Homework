1791456aea04943382bf49968a39a038287af764
theSkyKV <63708227+theSkyKV@users.noreply.github.com>
2021-01-20 16:04:35 +0300

    Update Task24.cs

--- Task24.cs
+++ Task24.cs
@@ -8,16 +8,15 @@ namespace CSLight
         static void Main(string[] args)
         {
             int account = 0;
-
-            Queue<int> customers = new Queue<int>();
-            CreateQueue(ref customers);
+            Queue<int> customers = CreateQueue();
 
             while (customers.Count > 0)
             {
                 Console.Clear();
                 int purchaseAmount = customers.Dequeue();
                 Console.WriteLine($"Добавлено {purchaseAmount} $ на счет.");
-                Console.WriteLine($"Всего денег на счету - {account += purchaseAmount} $");
+                account += purchaseAmount;
+                Console.WriteLine($"Всего денег на счету - {account} $");
                 Console.WriteLine("Нажмите любую кнопку для продолжения...");
                 Console.ReadKey(true);
             }
@@ -26,15 +25,18 @@ namespace CSLight
             Console.WriteLine($"Рабочий день окончен! Вы заработали {account} $");
         }
 
-        static void CreateQueue(ref Queue<int> queue)
+        static Queue<int> CreateQueue()
         {
+            Queue<int> queue = new Queue<int>();
             Random rand = new Random();
             int queueLength = rand.Next(5, 10);
-            
+
             for (var i = 0; i < queueLength; i++)
             {
                 queue.Enqueue(rand.Next(50, 1000));
             }
+
+            return queue;
         }
     }
 }
