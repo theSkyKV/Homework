54372a195ea4c8db9e2f01ba9ceb31727bff44ab
theSkyKV <63708227+theSkyKV@users.noreply.github.com>
2021-01-20 15:06:57 +0300

    Add files via upload

---
+++ Task23.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSLight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            Dictionary<string, string> vocabulary = new Dictionary<string, string>
+            {
+                ["Слово1"] = "Значение1",
+                ["Слово2"] = "Значение2",
+                ["Слово3"] = "Значение3"
+            };
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Введите слово: ");
+                userInput = Console.ReadLine();
+
+                if (vocabulary.ContainsKey(userInput))
+                {
+                    Console.WriteLine($"{userInput} - {vocabulary[userInput]}");
+                }
+                else
+                {
+                    Console.WriteLine("Такого слова нет...");
+                }
+                Console.WriteLine("Нажмите любую клавишу, чтобы продолжить...");
+                Console.ReadKey(true);
+            }
+        }
+    }
+}
