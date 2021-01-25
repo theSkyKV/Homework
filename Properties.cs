78e3d59e90050765626b3c822be0c2fc561d8431
theSkyKV <63708227+theSkyKV@users.noreply.github.com>
2021-01-25 11:26:48 +0300

    Update Properties.cs

--- Properties.cs
+++ Properties.cs
@@ -6,16 +6,18 @@ namespace Properties
     {
         static void Main(string[] args)
         {
-            Player player = new Player(20, 10);
-            Renderer renderer = new Renderer(player);
-            renderer.Draw();
+            Player player1 = new Player(20, 10);
+            Player player2 = new Player(10, 20);
+            Renderer renderer = new Renderer();
+            renderer.Draw(player1);
+            renderer.Draw(player2, '#');
         }
     }
 
     class Player
     {
         public int X { get; private set; }
-        public int Y{ get; private set; }
+        public int Y { get; private set; }
 
         public Player(int x, int y)
         {
@@ -26,16 +28,9 @@ namespace Properties
 
     class Renderer
     {
-        private Player _player;
-
-        public Renderer(Player player)
+        public void Draw(Player player, char symbol = '@')
         {
-            _player = player;
-        }
-
-        public void Draw(char symbol = '@')
-        {
-            Console.SetCursorPosition(_player.X, _player.Y);
+            Console.SetCursorPosition(player.X, player.Y);
             Console.Write(symbol);
         }
     }
