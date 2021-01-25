41e5424cc9e79a0535d18c64fb4f9dd5264f6b6f
theSkyKV <63708227+theSkyKV@users.noreply.github.com>
2021-01-25 10:14:54 +0300

    Add files via upload

---
+++ Properties.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player(20, 10);
+            Renderer renderer = new Renderer(player);
+            renderer.Draw();
+        }
+    }
+
+    class Player
+    {
+        public int X { get; private set; }
+        public int Y{ get; private set; }
+
+        public Player(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
+    class Renderer
+    {
+        private Player _player;
+
+        public Renderer(Player player)
+        {
+            _player = player;
+        }
+
+        public void Draw(char symbol = '@')
+        {
+            Console.SetCursorPosition(_player.X, _player.Y);
+            Console.Write(symbol);
+        }
+    }
+}
