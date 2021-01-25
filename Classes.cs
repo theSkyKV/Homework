235214bdf0260c9c9981f295794131038da26d04
theSkyKV <63708227+theSkyKV@users.noreply.github.com>
2021-01-25 09:23:14 +0300

    Add files via upload

---
+++ Classes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player(100, 20, 10);
+            player.ShowInfo();
+        }
+    }
+
+    class Player
+    {
+        private int _health;
+        private int _damage;
+        private int _armor;
+
+        public Player(int health, int damage, int armor)
+        {
+            _health = health;
+            _damage = damage;
+            _armor = armor;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("Характеристики персонажа:");
+            Console.Write($"Здоровье - {_health}\nУрон - {_damage}\nБроня - {_armor}");
+        }
+    }
+}
