40b45437e545e3668405d746c43d78f1bdfb49ad
theSkyKV <63708227+theSkyKV@users.noreply.github.com>
2021-02-08 16:31:29 +0300

    Update Autoservice.cs

--- Autoservice.cs
+++ Autoservice.cs
@@ -7,13 +7,9 @@ namespace AutoService
     {
         static void Main(string[] args)
         {
-            bool isOpen = true;
             AutoService autoService = new AutoService();
 
-            while (isOpen)
-            {
-                autoService.Work(ref isOpen);
-            }
+            autoService.Work();
         }
     }
 
@@ -30,95 +26,99 @@ namespace AutoService
             _accountBalance = 1000;
         }
 
-        public void Work(ref bool isOpen)
+        public void Work()
         {
+            bool isOpen = true;
 
-            Console.Clear();
-            Console.WriteLine($"Денег на счету: {_accountBalance} $");
-            Console.WriteLine("Выберите действие:");
-            Console.WriteLine("1 - Следующая машина, 2 - Заменить нужную деталь, 3 - Поставить другую деталь");
-            Console.WriteLine("4 - Показать детали на складе, 5 - Оплатить неустойку, 6 - Закрыть сервис.");
-            if (_carInService != null)
+            while (isOpen)
             {
-                _carInService.ShowInfo();
-            }
-            if (int.TryParse(Console.ReadLine(), out int userInput))
-            {
-                switch (userInput)
+                Console.Clear();
+                Console.WriteLine($"Денег на счету: {_accountBalance} $");
+                Console.WriteLine("Выберите действие:");
+                Console.WriteLine("1 - Следующая машина, 2 - Заменить нужную деталь, 3 - Поставить другую деталь");
+                Console.WriteLine("4 - Показать детали на складе, 5 - Оплатить неустойку, 6 - Закрыть сервис.");
+                if (_carInService != null)
                 {
-                    case 1:
-                        if (_carInService == null)
-                        {
-                            _carInService = new Car();
-                        }
-                        else
-                        {
-                            Console.WriteLine("Сначала закончите с предыдущим клиентом.");
-                        }
-                        break;
-                    case 2:
-                        if (_carInService == null)
-                        {
-                            Console.WriteLine("Нет машины для обслуживания");
-                            break;
-                        }
-
-                        bool isReplace = _warehouse.UseDetail(_carInService.BrokenDetailId, 1);
-                        if (isReplace)
-                        {
-                            Console.WriteLine("Деталь заменена. Клиент доволен.");
-                            var detail = _warehouse.GetDetailById(_carInService.BrokenDetailId);
-                            _accountBalance += detail.DetailPrice + detail.WorkPrice;
-                            _carInService = null;
-                        }
-                        break;
-                    case 3:
-                        if (_carInService == null)
-                        {
-                            Console.WriteLine("Нет машины для обслуживания");
-                            break;
-                        }
-
-                        _warehouse.UseDetail(_rand.Next(0, _warehouse.DetailsTypeAmount + 1), 1);
-                        if (PayFine(1000))
-                        {
-                            Console.WriteLine("Клиент не доволен.");
-                            _carInService = null;
-                        }
-                        else
-                        {
-                            Console.WriteLine("У вас нет денег для оплаты неустойки. Вы банкрот");
-                            isOpen = false;
-                        }
-                        break;
-                    case 4:
-                        _warehouse.ShowInfo();
-                        break;
-                    case 5:
-                        if (_carInService == null)
-                        {
-                            Console.WriteLine("Нет машины для обслуживания");
-                            break;
-                        }
-
-                        PayFine(500);
-                        _carInService = null;
-                        break;
-                    case 6:
-                        isOpen = false;
-                        break;
-                    default:
-                        Console.WriteLine("Некорректный ввод.");
-                        break;
+                    _carInService.ShowInfo();
                 }
-            }
-            else
-            {
-                Console.WriteLine("Некорректный ввод.");
-            }
+                if (int.TryParse(Console.ReadLine(), out int userInput))
+                {
+                    switch (userInput)
+                    {
+                        case 1:
+                            if (_carInService == null)
+                            {
+                                _carInService = new Car();
+                            }
+                            else
+                            {
+                                Console.WriteLine("Сначала закончите с предыдущим клиентом.");
+                            }
+                            break;
+                        case 2:
+                            if (_carInService == null)
+                            {
+                                Console.WriteLine("Нет машины для обслуживания");
+                                break;
+                            }
 
-            Console.WriteLine("Нажмите любую клавишу для продолжения...");
-            Console.ReadKey(true);
+                            bool isReplace = _warehouse.UseDetail(_carInService.BrokenDetailId, 1);
+                            if (isReplace)
+                            {
+                                Console.WriteLine("Деталь заменена. Клиент доволен.");
+                                var detail = _warehouse.GetDetailById(_carInService.BrokenDetailId);
+                                _accountBalance += detail.DetailPrice + detail.WorkPrice;
+                                _carInService = null;
+                            }
+                            break;
+                        case 3:
+                            if (_carInService == null)
+                            {
+                                Console.WriteLine("Нет машины для обслуживания");
+                                break;
+                            }
+
+                            _warehouse.UseDetail(_rand.Next(0, _warehouse.DetailsTypeAmount + 1), 1);
+                            if (PayFine(1000))
+                            {
+                                Console.WriteLine("Клиент не доволен.");
+                                _carInService = null;
+                            }
+                            else
+                            {
+                                Console.WriteLine("У вас нет денег для оплаты неустойки. Вы банкрот");
+                                isOpen = false;
+                            }
+                            break;
+                        case 4:
+                            _warehouse.ShowInfo();
+                            break;
+                        case 5:
+                            if (_carInService == null)
+                            {
+                                Console.WriteLine("Нет машины для обслуживания");
+                                break;
+                            }
+
+                            PayFine(500);
+                            _carInService = null;
+                            break;
+                        case 6:
+                            isOpen = false;
+                            break;
+                        default:
+                            Console.WriteLine("Некорректный ввод.");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Некорректный ввод.");
+                }
+
+                Console.WriteLine("Нажмите любую клавишу для продолжения...");
+                Console.ReadKey(true);
+            }
         }
 
         public bool PayFine(int fine)
@@ -162,19 +162,19 @@ namespace AutoService
 
     class Warehouse
     {
-        private List<Detail> _details;
+        private List<Cell> _cells;
 
         public int DetailsTypeAmount
         {
             get
             {
-                return _details.Count;
+                return _cells.Count;
             }
         }
 
         public Warehouse()
         {
-            _details = new List<Detail>();
+            _cells = new List<Cell>();
             for (var i = 0; i < 10; i++)
             {
                 AddDetail();
@@ -183,25 +183,25 @@ namespace AutoService
 
         public void AddDetail(int id = -1, int amount = 0)
         {
-            foreach (var detail in _details)
+            foreach (var cell in _cells)
             {
-                if (detail.Id == id)
+                if (cell.Detail.Id == id)
                 {
-                    detail.Add(amount);
+                    cell.PutDetail(amount);
                     return;
                 }
             }
 
-            _details.Add(new Detail());
+            _cells.Add(new Cell(new Detail(), 5));
         }
 
         public bool UseDetail(int id, int amount)
         {
-            foreach (var detail in _details)
+            foreach (var cell in _cells)
             {
-                if (detail.Id == id)
+                if (cell.Detail.Id == id)
                 {
-                    detail.Remove(amount);
+                    cell.TakeDetail(amount);
                     return true;
                 }
             }
@@ -211,14 +211,23 @@ namespace AutoService
 
         public Detail GetDetailById(int id)
         {
-            return _details[id];
+            foreach (var cell in _cells)
+            {
+                if (cell.Detail.Id == id)
+                {
+                    return cell.Detail;
+                }
+            }
+
+            Console.WriteLine("Такой детали нет.");
+            return null;
         }
 
         public void ShowInfo()
         {
-            foreach (var detail in _details)
+            foreach (var cell in _cells)
             {
-                Console.WriteLine($"{detail.Id} | {detail.Name} | {detail.DetailPrice} | {detail.WorkPrice} | {detail.Amount}");
+                Console.WriteLine($"{cell.Detail.Id} | {cell.Detail.Name} | {cell.Detail.DetailPrice} | {cell.Detail.WorkPrice} | {cell.Amount}");
             }
         }
     }
@@ -232,7 +241,6 @@ namespace AutoService
         public string Name { get; private set; }
         public int DetailPrice { get; private set; }
         public int WorkPrice { get; private set; }
-        public int Amount { get; private set; }
 
         public Detail()
         {
@@ -240,15 +248,32 @@ namespace AutoService
             Name = "Detail" + Id;
             DetailPrice = _rand.Next(30, 200);
             WorkPrice = _rand.Next(20, 70);
-            Amount = _rand.Next(5, 15);
+        }
+    }
+
+    class Cell
+    {
+        public Detail Detail { get; private set; }
+        public int Amount { get; private set; }
+
+        public Cell(Detail detail, int amount)
+        {
+            Detail = detail;
+            Amount = amount;
         }
 
-        public void Add(int amount)
+        public void PutDetail(int amount)
         {
+            if (amount < 0)
+            {
+                Console.WriteLine("Неверное количество деталей.");
+                return;
+            }
+
             Amount += amount;
         }
 
-        public void Remove(int amount)
+        public void TakeDetail(int amount)
         {
             if (amount < 0)
             {
