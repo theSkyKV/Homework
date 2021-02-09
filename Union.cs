7f7f1fd6e14a7ff13b40a2fc78e621c6e2d930f0
theSkyKV <63708227+theSkyKV@users.noreply.github.com>
2021-02-09 16:35:37 +0300

    Update Union.cs

--- Union.cs
+++ Union.cs
@@ -18,27 +18,20 @@ namespace Union
         private static Random _rand = new Random();
         private List<Soldier> _soldiers1;
         private List<Soldier> _soldiers2;
-        private List<string> _armings;
-        private List<string> _militaryRanks;
 
         public Database()
         {
             _soldiers1 = new List<Soldier>();
             _soldiers2 = new List<Soldier>();
-            _armings = new List<string>() { "АК-74", "СВД", "Макар" };
-            _militaryRanks = new List<string>() { "рядовой", "ефрейтор", "сержант", "лейтенант", "капитан" };
 
             for (var i = 0; i < 30; i++)
             {
                 string name = Convert.ToChar(_rand.Next(65, 70)) + "name" + i;
-                string arming = _armings[_rand.Next(0, _armings.Count)];
-                string militaryRank = _militaryRanks[_rand.Next(0, _militaryRanks.Count)];
-                int serviceLife = _rand.Next(1, 25);
 
                 if (i % 2 == 0)
-                    _soldiers1.Add(new Soldier(name, arming, militaryRank, serviceLife));
+                    _soldiers1.Add(new Soldier(name));
                 else
-                    _soldiers2.Add(new Soldier(name, arming, militaryRank, serviceLife));
+                    _soldiers2.Add(new Soldier(name));
             }
         }
 
@@ -60,16 +53,10 @@ namespace Union
     class Soldier
     {
         public string Name { get; private set; }
-        public string Arming { get; private set; }
-        public string MilitaryRank { get; private set; }
-        public int ServiceLife { get; private set; }
 
-        public Soldier(string name, string arming, string militaryRank, int serviceLife)
+        public Soldier(string name)
         {
             Name = name;
-            Arming = arming;
-            MilitaryRank = militaryRank;
-            ServiceLife = serviceLife;
         }
     }
 }
