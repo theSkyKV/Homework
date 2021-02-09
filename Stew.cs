cc239451f89274b027d85aad2f7f24f07d472241
theSkyKV <63708227+theSkyKV@users.noreply.github.com>
2021-02-09 15:00:38 +0300

    Add files via upload

---
+++ Stew.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Stew
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Warehouse warehouse = new Warehouse();
+            warehouse.GetExpiredProduct(2021);
+        }
+    }
+
+    class Warehouse
+    {
+        private static Random _rand = new Random();
+        private List<Stew> _stew;
+
+        public Warehouse()
+        {
+            _stew = new List<Stew>();
+
+            for (var i = 0; i < 30; i++)
+            {
+                string name = Convert.ToChar(_rand.Next(65, 91)) + "name" + i;
+                int productionYear = _rand.Next(2000, 2021);
+                int shelfLife = _rand.Next(5, 25);
+                _stew.Add(new Stew(name, productionYear, shelfLife));
+            }
+        }
+
+        public void GetExpiredProduct(int currentYear)
+        {
+            var expiredStew = _stew.Where(stew => stew.ProductionYear + stew.ShelfLife < currentYear);
+
+            if (expiredStew.Count() > 0)
+            {
+                ShowWarehouse(expiredStew);
+            }
+            else
+            {
+                Console.WriteLine("Продуктов с истекшим сроком годности нет");
+            }
+        }
+
+        public void ShowWarehouse(IEnumerable<Stew> stew)
+        {
+            foreach (var item in stew)
+            {
+                Console.WriteLine($"{item.Name} | {item.ProductionYear} | {item.ShelfLife}");
+            }
+        }
+    }
+
+    class Stew
+    {
+        public string Name { get; private set; }
+        public int ProductionYear { get; private set; }
+        public int ShelfLife { get; private set; }
+
+        public Stew(string name, int productionYear, int shelfLife)
+        {
+            Name = name;
+            ProductionYear = productionYear;
+            ShelfLife = shelfLife;
+        }
+    }
+}
